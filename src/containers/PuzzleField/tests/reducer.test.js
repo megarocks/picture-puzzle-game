49bbf982d80f70d40b1ce3e77e829d12a80ec4f1
Vitaly <vitaly.kiktiov@gmail.com>
2018-03-04 20:58:24 +0200

    game state reducer on immutable js

--- src/containers/PuzzleField/tests/reducer.test.js
+++ src/containers/PuzzleField/tests/reducer.test.js
@@ -1,53 +1,48 @@
-import gameStateReducer from '../reducer'
+import { fromJS } from 'immutable';
 
+import gameStateReducer from '../reducer'
 import { startNewGame, gameTurn } from '../actions'
 
 
 describe('gameStateReducer', () => {
   let state
   beforeEach(() => {
-    state = {
+    state = fromJS({
       fieldSideSize: 500,
       puzzlesPerSide: 4,
       puzzles: []
-    }
-  });
-
-  it('should return the initial state', () => {
-    expect(gameStateReducer(undefined, {})).toEqual({});
+    })
   });
 
   it('should have puzzlesPerSide*puzzlesPerSide puzzles when game started', () => {
-      const expectedResult = state.puzzlesPerSide * state.puzzlesPerSide
-      expect(gameStateReducer(state, startNewGame(true)).puzzles).toHaveLength(expectedResult)
+    const puzzlesPerSide = state.get('puzzlesPerSide');
+    const expectedResult = puzzlesPerSide * puzzlesPerSide
+    expect(gameStateReducer(state, startNewGame(true)).get('puzzles')).toHaveLength(expectedResult)
   })
 
   it('should give capability to start game already solved', () => {
-
     const solvedGame = gameStateReducer(state, startNewGame(false))
-    expect(solvedGame.puzzles[0].fieldRow).toEqual(state.puzzlesPerSide - 1)
-    expect(solvedGame.puzzles[0].fieldColumn).toEqual(state.puzzlesPerSide - 1)
-    expect(solvedGame.puzzles[1].fieldRow).toEqual(0)
-    expect(solvedGame.puzzles[1].fieldColumn).toEqual(0)
-    expect(solvedGame.puzzles[solvedGame.puzzles.length - 1].fieldRow).toEqual(state.puzzlesPerSide - 1)
-    expect(solvedGame.puzzles[solvedGame.puzzles.length - 1].fieldColumn).toEqual(state.puzzlesPerSide - 2)
+    expect(solvedGame.get('puzzles')[0].fieldRow).toEqual(state.get('puzzlesPerSide') - 1)
+    expect(solvedGame.get('puzzles')[0].fieldColumn).toEqual(state.get('puzzlesPerSide') - 1)
+    expect(solvedGame.get('puzzles')[1].fieldRow).toEqual(0)
+    expect(solvedGame.get('puzzles')[1].fieldColumn).toEqual(0)
+    expect(solvedGame.get('puzzles')[solvedGame.get('puzzles').length - 1].fieldRow).toEqual(state.get('puzzlesPerSide') - 1)
+    expect(solvedGame.get('puzzles')[solvedGame.get('puzzles').length - 1].fieldColumn).toEqual(state.get('puzzlesPerSide') - 2)
   })
 
   it('should be no changes to puzzles when turn on unclickable puzzle', () => {
-
     const solvedGameState = gameStateReducer(state, startNewGame(false))
-    const lastPuzzleBeforeTurn = solvedGameState.puzzles[1]
+    const lastPuzzleBeforeTurn = solvedGameState.get('puzzles')[1]
     const stateAfterClickOnLastPuzzle = gameStateReducer(solvedGameState, gameTurn(lastPuzzleBeforeTurn))
-    const lastPuzzleAfterTurn = stateAfterClickOnLastPuzzle.puzzles[1]
+    const lastPuzzleAfterTurn = stateAfterClickOnLastPuzzle.get('puzzles')[1]
     expect(lastPuzzleAfterTurn).toEqual(lastPuzzleBeforeTurn)
   })
 
   it('should swap between puzzles when turn on clickable puzzle', () => {
-
     const solvedGameState = gameStateReducer(state, startNewGame(false))
-    const lastPuzzleBeforeTurn = solvedGameState.puzzles[solvedGameState.puzzles.length - 1]
+    const lastPuzzleBeforeTurn = solvedGameState.get('puzzles')[solvedGameState.get('puzzles').length - 1]
     const stateAfterClickOnLastPuzzle = gameStateReducer(solvedGameState, gameTurn(lastPuzzleBeforeTurn))
-    const lastPuzzleAfterTurn = stateAfterClickOnLastPuzzle.puzzles[solvedGameState.puzzles.length - 1]
+    const lastPuzzleAfterTurn = stateAfterClickOnLastPuzzle.get('puzzles')[solvedGameState.get('puzzles').length - 1]
     expect(lastPuzzleAfterTurn).not.toEqual(lastPuzzleBeforeTurn)
   })
 
